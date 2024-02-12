98eb41735a3a81436d305a3836c503cea7cf2458
Lyramakes <blyra05@gmail.com>
2024-02-12 11:51:24 -0500

    Add Java project

---
+++ LethalCraft/src/client/java/info/lbrown/mc/lethalcompany/LethalCompanyClient.java
@@ -0,0 +1,10 @@
+package info.lbrown.mc.lethalcompany;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LethalCompanyClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
